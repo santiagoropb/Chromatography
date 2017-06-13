892339eb9eff68e22b26557396b9060add6c8dbe
Santiago <santiago@gmail.com>
2017-06-13 06:49:08 -0400

    Adding functionality for parsing binding models inputs

--- pycadet/model/binding_model.py
+++ pycadet/model/binding_model.py
@@ -10,35 +10,15 @@ logger = logging.getLogger(__name__)
 
 class BindingModel(abc.ABC):
 
-    def __init__(self, inputs):
+    def __init__(self):
 
-        # parse inputs
-        if isinstance(inputs, dict):
-            agrs = inputs
-        elif isinstance(inputs, six.string_types):
-            if ".yml" in inputs or ".yaml" in inputs:
-                with open(inputs, 'r') as f:
-                    args = yaml.load(f)
-            else:
-                raise RuntimeError('File format not implemented yet. Try .yml or .yaml')
-        else:
-            raise RuntimeError('inputs must be a dictionary or a file')
-
-        # extract scalar params
+        # define scalar params container
         self._scalar_params = dict()
-        sparams = args.get('scalar parameters')
-        if sparams is not None:
-            for name, val in sparams.items():
-                self._scalar_params[name] = val
 
-        # extract components and component indexed parameters
-        self._components = list()
+        # define components and component indexed parameters
+        self._components = set()
+        self._indexed_params = dict()
 
-        # dict_comp = args.get('components')
-
-        # component_idx_param_names = []
-        # if dict_comp is not None:
-        #    for
 
     @property
     def num_components(self):
@@ -53,6 +33,33 @@ class BindingModel(abc.ABC):
         Returns number of scalar parameters
         """
         return len(self._scalar_params)
+
+    @property
+    def num_indexed_parameters(self):
+        """
+        Returns number of indexed parameters
+        """
+        return len(self._indexed_params)
+
+    @staticmethod
+    def _parse_inputs(inputs):
+        """
+        Parse inputs
+        :param inputs: filename or dictionary with inputs to the binding model
+        :return: dictionary with parsed inputs
+        """
+        if isinstance(inputs, dict):
+            agrs = inputs
+        elif isinstance(inputs, six.string_types):
+            if ".yml" in inputs or ".yaml" in inputs:
+                with open(inputs, 'r') as f:
+                    args = yaml.load(f)
+            else:
+                raise RuntimeError('File format not implemented yet. Try .yml or .yaml')
+        else:
+            raise RuntimeError('inputs must be a dictionary or a file')
+
+        return args
 
     @abc.abstractmethod
     def add_component_indexed_param(self, comp_id, name, value):
@@ -138,12 +145,77 @@ class SMAModel(BindingModel):
 
     def __init__(self, inputs):
 
+        # call binding model constructor
+        super().__init__()
+
         self._registered_parameters = set()
         self._registered_parameters.add('kads')
         self._registered_parameters.add('kdes')
         self._registered_parameters.add('upsilon')
         self._registered_parameters.add('sigma')
         self._registered_parameters.add('Lambda')
+        self._registered_parameters.add('refq')
+        self._registered_parameters.add('refc')
+
+        args = self._parse_inputs(inputs)
+
+        sparams = args.get('scalar parameters')
+        if sparams is not None:
+            for name, val in sparams.items():
+                assert name in self._registered_parameters, "{} is not a SMA parameter".format(name)
+                self._scalar_params[name] = val
+
+        dict_comp = args.get('components')
+        has_params = False
+        if dict_comp is not None:
+
+            if isinstance(dict_comp,list):
+                to_loop = dict_comp
+            else:
+                to_loop = dict_comp.keys()
+                has_params = True
+
+            for comp_id in to_loop:
+                if comp_id in self._components:
+                    msg = "Component {} being overwritten".format(comp_id)
+                    logger.warning(msg)
+                self._components.add(comp_id)
+        else:
+            logger.error("SMA no components found")
+            raise RuntimeError('SMA model needs to know the components')
+
+        self._indexed_params = pd.DataFrame(index=self._components,
+                                            columns=list(self._registered_parameters))
+
+        self._indexed_params.index.name = 'component id'
+        self._indexed_params.columns.name = 'parameters'
+
+        if has_params:
+            for comp_id, params in dict_comp.items():
+                for parameter, value in params.items():
+                    assert parameter in self._registered_parameters, "{} is not a SMA parameter".format(parameter)
+                    self._indexed_params[parameter][comp_id] = value
+
+    def add_component_indexed_param(self, comp_id, name, value):
+
+        if isinstance(comp_id, list) and isinstance(name, six.string_types) and isinstance(value, list):
+
+            if name not in self._registered_parameters:
+                raise RuntimeError("{} is not a SMA parameter".format(name))
+
+            if len(comp_id) != len(value):
+                raise RuntimeError("The arrays must be equal size")
+
+            for i,cid in enumerate(comp_id):
+                if cid not in self._components:
+                    raise RuntimeError("{} not a component".format(cid))
+                self._indexed_params[name][cid] = value[i]
+
+
+
+
+
+
 
 
 
