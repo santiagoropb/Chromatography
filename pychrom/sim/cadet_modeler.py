6f1574f872e61dc19fb8652c6e9e1f307284a1d5
Santiago <santiago@gmail.com>
2017-06-23 09:14:34 -0400

    Add base to start modeler classes

---
+++ pychrom/sim/cadet_modeler.py
@@ -0,0 +1,34 @@
+from pychrom.model.chromatograpy_model import GRModel
+
+
+class CadetModeler(object):
+
+    def __init__(self, model):
+        """
+
+        :param model: GRModel
+        """
+        self._model = model
+
+    def discretize(self, ncol, npar, **kwargs):
+        """
+
+        :param ncol:
+        :param npar:
+        :param kwargs:
+        :return:
+        """
+
+        print("TODO")
+
+    def run_sim(self, retrive='in_out', keep_file=False, **kwargs):
+        """
+
+        :param retrive:
+        :param keep_file:
+        :param kwargs:
+        :return:
+        """
+        print("TODO")
+
+    
