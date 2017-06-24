49645bf5c5b448d31f4d04aa70b612a785e75204
Santiago <santiago@gmail.com>
2017-06-23 22:43:15 -0400

    turn off openmp for sundials

--- build_cadet/get.sundials.sh
+++ build_cadet/get.sundials.sh
@@ -43,7 +43,7 @@ rm -rf $sundials.tar
 # building sundials
 mkdir build_sundials
 cd build_sundials
-cmake -DCMAKE_INSTALL_PREFIX=$PWD/cadet-install/Libs/sundials -DEXAMPLES_ENABLE=OFF -DOPENMP_ENABLE=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS=-fPIC ../$sundials
+cmake -DCMAKE_INSTALL_PREFIX=$PWD/cadet-install/Libs/sundials -DEXAMPLES_ENABLE=OFF -DOPENMP_ENABLE=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS=-fPIC -DNO_OMP=1 ../$sundials
 make
 make install
 cd ../..
